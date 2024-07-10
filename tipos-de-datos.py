9495cb21c122d30e5f47fb8606447b5e4cbc31f0
Jheyson Galvis <tecnojheyson@gmail.com>
2024-07-10 09:32:43 -0500

    se inicializa el repositorio bootcamp de Python

---
+++ tipos-de-datos.py
@@ -0,0 +1,20 @@
+"string" #string se utiliza para tipos de datos texto
+'string' #así también podemos escribir un string
+"""string""" #y así también podemos escribir un string
+'''string''' #o por ejemplo así también
+
+"""Tus datos son:
+    nombre: Jheyson
+    apellidos: Galvis"""
+'''Tus datos son:
+    nombre: Jheyson
+    apellidos: Galvis'''
+
+#En Python tenemos diferentes tipos de números:
+4 #entero o tipo de dato entero
+40.2 #flotante o tipo de dato float
+
+#También tenemos el tipo de dato simple (bool) true o false
+
+False #False o tipo de dato bool, en Python las escribirmos con mayúsculas
+True #True o tipo de dato bool, en Python las escribirmos con mayúsculas
