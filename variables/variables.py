bb30ef1fadb6a823f9c7ca039e9a0238f0633af0
Jheyson Galvis <tecnojheyson@gmail.com>
2024-07-10 12:19:38 -0500

    variables en Python

---
+++ variables/variables.py
@@ -0,0 +1,6 @@
+#Definiendo una variable con CamelCase (Recomendado para Javascript)
+nombreCompletoDeTuProfe = "Jheyson Galvis" #CamelCase sirve para definir variables
+
+#Definiendo una variable con SnakeCase (Recomendado para Python)
+nombre_completo_de_tu_profe = "Jheyson Galvis " #Snake Case
+
